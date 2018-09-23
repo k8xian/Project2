34631da115080a60898c033dc1b83aed8627a581
Ciwonie <ciwonie@gmail.com>
2018-09-22 21:48:03 -0500

    updating the DOM with real-time space weather changes from NOAA

--- public/data/data.js
+++ public/data/data.js
@@ -1,119 +1,119 @@
 var event = [
   {
-    "name": "event one",
-    "image": "LINK",
-    "fact": "I like Turtles",
-    "option": {
+    name: "event one",
+    image: "LINK",
+    fact: "I like Turtles",
+    option: {
       "option-a": "option a",
       "option-b": "option b",
       "option-c": "option c"
     },
-    "result": {
+    result: {
       "result-a": "result a",
       "result-b": "result b",
       "result-c": "result c"
     },
-    "multiplier": {
+    multiplier: {
       "multiplier-a": "math-a",
       "multiplier-b": "math-b",
       "multiplier-c": "math-c"
     }
   },
   {
-    "name": "event two",
-    "image": "LINK",
-    "fact": "I like Turtles",
-    "option": {
+    name: "event two",
+    image: "LINK",
+    fact: "I like Turtles",
+    option: {
       "option-a": "option a",
       "option-b": "option b",
       "option-c": "option c"
     },
-    "result": {
+    result: {
       "result-a": "result a",
       "result-b": "result b",
       "result-c": "result c"
     },
-    "multiplier": {
+    multiplier: {
       "multiplier-a": "math-a",
       "multiplier-b": "math-b",
       "multiplier-c": "math-c"
     }
   },
   {
-    "name": "event three",
-    "image": "LINK",
-    "fact": "I like Turtles",
-    "option": {
+    name: "event three",
+    image: "LINK",
+    fact: "I like Turtles",
+    option: {
       "option-a": "option a",
       "option-b": "option b",
       "option-c": "option c"
     },
-    "result": {
+    result: {
       "result-a": "result a",
       "result-b": "result b",
       "result-c": "result c"
     },
-    "multiplier": {
+    multiplier: {
       "multiplier-a": "math-a",
       "multiplier-b": "math-b",
       "multiplier-c": "math-c"
     }
   },
   {
-    "name": "event four",
-    "image": "LINK",
-    "fact": "I like Turtles",
-    "option": {
+    name: "event four",
+    image: "LINK",
+    fact: "I like Turtles",
+    option: {
       "option-a": "option a",
       "option-b": "option b",
       "option-c": "option c"
     },
-    "result": {
+    result: {
       "result-a": "result a",
       "result-b": "result b",
       "result-c": "result c"
     },
-    "multiplier": {
+    multiplier: {
       "multiplier-a": "math-a",
       "multiplier-b": "math-b",
       "multiplier-c": "math-c"
     }
   },
   {
-    "name": "event five",
-    "image": "LINK",
-    "fact": "I like Turtles",
-    "option": {
+    name: "event five",
+    image: "LINK",
+    fact: "I like Turtles",
+    option: {
       "option-a": "option a",
       "option-b": "option b",
       "option-c": "option c"
     },
-    "result": {
+    result: {
       "result-a": "result a",
       "result-b": "result b",
       "result-c": "result c"
     },
-    "multiplier": {
+    multiplier: {
       "multiplier-a": "math-a",
       "multiplier-b": "math-b",
       "multiplier-c": "math-c"
     }
   },
   {
-    "name": "event six",
-    "image": "LINK",
-    "fact": "I like Turtles",
-    "option": {
+    name: "event six",
+    image: "LINK",
+    fact: "I like Turtles",
+    option: {
       "option-a": "option a",
       "option-b": "option b",
       "option-c": "option c"
     },
-    "result": {
+    result: {
       "result-a": "result a",
       "result-b": "result b",
       "result-c": "result c"
     },
-    "multiplier": {
+    multiplier: {
       "multiplier-a": "math-a",
       "multiplier-b": "math-b",
       "multiplier-c": "math-c"
@@ -123,60 +123,60 @@ var event = [
 
 var checkpoint = [
   {
-    "name": "checkpoint one",
-    "image": "url",
-    "fact": "stuff goes here",
-    "option": {
+    name: "checkpoint one",
+    image: "url",
+    fact: "stuff goes here",
+    option: {
       "option-a": "option a",
       "option-b": "option b",
       "option-c": "option c"
     },
-    "result": {
+    result: {
       "result-a": "result a",
       "result-b": "result b",
       "result-c": "result c"
     },
-    "multiplier": {
+    multiplier: {
       "multiplier-a": "math-a",
       "multiplier-b": "math-b",
       "multiplier-c": "math-c"
     }
   },
   {
-    "name": "checkpoint two",
-    "image": "url",
-    "fact": "stuff goes here",
-    "option": {
+    name: "checkpoint two",
+    image: "url",
+    fact: "stuff goes here",
+    option: {
       "option-a": "option a",
       "option-b": "option b",
       "option-c": "option c"
     },
-    "result": {
+    result: {
       "result-a": "result a",
       "result-b": "result b",
       "result-c": "result c"
     },
-    "multiplier": {
+    multiplier: {
       "multiplier-a": "math-a",
       "multiplier-b": "math-b",
       "multiplier-c": "math-c"
     }
   },
   {
-    "name": "checkpoint three",
-    "image": "url",
-    "fact": "stuff goes here",
-    "option": {
+    name: "checkpoint three",
+    image: "url",
+    fact: "stuff goes here",
+    option: {
       "option-a": "option a",
       "option-b": "option b",
       "option-c": "option c"
     },
-    "result": {
+    result: {
       "result-a": "result a",
       "result-b": "result b",
       "result-c": "result c"
     },
-    "multiplier": {
+    multiplier: {
       "multiplier-a": "math-a",
       "multiplier-b": "math-b",
       "multiplier-c": "math-c"
